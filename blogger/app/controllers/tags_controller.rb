dc79c84da362ee91aabb2f0ceea2afedc92e09dd
Stephen Dang <dragondang19@gmail.com>
2014-05-26 18:37:08 -0400

    Iteration 3

---
+++ blogger/app/controllers/tags_controller.rb
@@ -0,0 +1,15 @@
+class TagsController < ApplicationController
+	def show
+		@tag = Tag.find(params[:id])
+	end
+	def index
+		@tag = Tag.all
+	end
+	def destroy
+		@tag = Tag.find(params[:id])
+		@tag.destroy
+		flash.notice = "Tag '#{@tag.name} deleted!"
+		redirect_to tags_path
+	end
+
+end
